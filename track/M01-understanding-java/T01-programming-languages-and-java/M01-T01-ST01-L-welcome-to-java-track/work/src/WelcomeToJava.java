9ef5470e257a05e16ceaf9b6eae1dd2d784d8cfe
Varshini <varshinibelkere@gmail.com>
2026-09-02 11:01:26 +0530

    this is my first update

--- track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/WelcomeToJava.java
+++ track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/WelcomeToJava.java
@@ -1,5 +1,6 @@
 public class WelcomeToJava {
     public static void main(String[] args) {
-       //Write your code here
+         //Write your code here
+         System.out.println("I am a java track");
     }
 }
